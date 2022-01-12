5f26777e810c8398c04e793189873eba1ed74eb5
sudonnoh <sudonnoh@gmail.com>
2022-01-12 10:27:16 +0900

    NestedSerializerMixin update

--- admin/API/serializers.py
+++ admin/API/serializers.py
@@ -1,12 +1,53 @@
 from rest_framework import serializers
-from brand.API.serializers import BrandSerializer
+# from brand.API.serializers import BrandSerializer
 
 from authentication.models import User
 from brand.models import Brand
+from django.contrib.auth.models import Group
 
 
+class AdminGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = "__all__"
+        
+        
+class AdminBrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = [
+            'id',
+            'brand',
+            'brand_address',
+            'brand_type',
+            'brand_phone_number',
+            'brand_email',
+            'brand_homepage',
+            'brand_description',
+            'brand_logo'
+        ]
+        
+        
 class AdminUserSerializer(serializers.ModelSerializer):
-    brand = BrandSerializer()
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+            'phone_number',
+            'groups',
+            'brand',
+            'last_login',
+            'is_active',
+            'created_at',
+            'updated_at',
+        ]
+
+    
+class NestedAdminUserSerializer(serializers.ModelSerializer):
+    brand = AdminBrandSerializer()
+    groups = AdminGroupSerializer(many=True)
     
     class Meta:
         model = User
@@ -22,30 +63,4 @@ class AdminUserSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
         ]
-        # depth = 1
-        
-    # def update(self, instance, validated_data):
-    #     print("\n\n validated_data : ", validated_data)
-    #     print("\n\n validated_data : ", validated_data['brand'])
-    #     print("\n\n instance : ", instance, "\n\n")
-        
-    #     brand = validated_data.pop('brand')
-        
-    #     instance.brand = brand.id
-    #     return instance
-
-
-class AdminBrandSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Brand
-        fields = [
-            'id',
-            'brand',
-            'brand_address',
-            'brand_type',
-            'brand_phone_number',
-            'brand_email',
-            'brand_homepage',
-            'brand_description',
-            'brand_logo'
-        ]
+    
--- admin/API/views.py
+++ admin/API/views.py
@@ -2,15 +2,25 @@ from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import serializers, viewsets
 
-from .serializers import AdminBrandSerializer, AdminUserSerializer
+from .serializers import AdminBrandSerializer, AdminUserSerializer, NestedAdminUserSerializer
 from .renderers import AdminUserJSONRenderer, AdminBrandJSONRenderer
 from authentication.models import User
 from brand.models import Brand
 from core.permissions import IsSystemAdmin, IsSiteAdmin
 
+
+class NestedSrializerMixin(viewsets.ModelViewSet):
+    read_serializer_class = None
+    
+    def get_serializer_class(self):
+        if self.request.method.lower() =="get":
+            return self.read_serializer_class
+        return self.serializer_class
+
+
 # Admin이 User의 정보를 생성, 조회, 수정, 삭제 할 수 있어야 함.
 # 따라서 모든 가능을 갖고 있는 ModelViewSet으로 설정할 예정입니다.
-class AdminUserViewSet(viewsets.ModelViewSet):
+class AdminUserViewSet(NestedSrializerMixin):
     
     queryset = User.objects.all()
     serializer_class = AdminUserSerializer
@@ -19,6 +29,7 @@ class AdminUserViewSet(viewsets.ModelViewSet):
         (IsSystemAdmin|IsSiteAdmin),
     )
     renderer_classes = (AdminUserJSONRenderer,)
+    read_serializer_class = NestedAdminUserSerializer
     
     
 class AdminBrandViewSet(viewsets.ModelViewSet):
@@ -29,3 +40,5 @@ class AdminBrandViewSet(viewsets.ModelViewSet):
         (IsSystemAdmin|IsSiteAdmin)
     )
     renderer_classes = (AdminBrandJSONRenderer,)
+    
+

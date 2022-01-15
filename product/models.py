9f9b181058e8bd428e1427d21ec5782024c40a38
sudonnoh <sudonnoh@gmail.com>
2022-01-15 14:38:04 +0900

    2021.01.15 update

--- product/models.py
+++ product/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 from core.models import TimestampedModel
 
-# Create your models here.
+
 class Category(models.Model):
-    category = models.CharField(max_length=255)
+    category = models.CharField(unique=True, max_length=128)
     
     
 class SubCategory(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    sub_category = models.CharField(max_length=255)
+    category = models.ForeignKey(unique=True, max_length=128)
+    subcategory = models.CharField(unique=True, max_length=128)
     
     
-# Vendor가 올리는 것들은 Brand가 들어가도록 한다.
-class Product(TimestampedModel):
-    brand = models.ForeignKey('brand.Brand', on_delete=models.CASCADE)
-    product_number = models.CharField(max_length=255)
-    product_name = models.CharField(max_length=255)
+class Product(models.Model):
+    brand_id = models.ForeignKey('brand.Brand', on_delete=models.PROTECT)
+    name = models.CharField(max_length=255, unique=True)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.PROTECT)
+    product_color = models.CharField(max_length=255)
+    product_number = models.CharField(max_length=128, unique=True)
     
+    def update(self):
+        
+    def save(self, *args, **kwargs):
+        

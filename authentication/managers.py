c36c48baa563f4569de03f543607cc332cfa9645
sudonnoh <sudonnoh@gmail.com>
2022-01-18 14:52:21 +0900

    command update

--- authentication/managers.py
+++ authentication/managers.py
@@ -19,10 +19,8 @@ class UserManager(BaseUserManager):
             email = self.normalize_email(email),
             **extra_fields
         )
-        print('password setting !!!!!!!!!!!!!!!!!')
-        print(password)
         user.set_password(password)
-        print('second password !!!!!!!!!!!!!!!', user.password)
+
         user.save()
         return user
 
